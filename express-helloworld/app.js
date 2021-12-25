a75cfd790a1dcc3dce44849df6589b37a585c84f
turgut-fincan <turgutfincan@gmail.com>
2021-12-25 20:11:32 +0300

    add mars commit 2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,10 @@ var express = require('express');
 app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
+});
   
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+app.get('/mars', function (req, res) {
+  res.send('Hello MARS!\n');
 });
 
 app.listen(8080, function () {
